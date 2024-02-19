62826605577f42ccc2a3f0c422288723159ab66f
thiagola92 <thiagola92@gmail.com>
2024-02-19 19:41:47 -0300

    Use class to hold data about each bone (like Godot code)

--- addons/la_ik/nodes/la_ik_ccdik.gd
+++ addons/la_ik/nodes/la_ik_ccdik.gd
@@ -3,52 +3,24 @@ class_name LaIKCCDIK
 extends LaIK
 
 
-@export var bone_root: LaBone:
+@export var root_bone: LaBone:
 	set(r):
-		bone_root = r
-		chain = []
+		root_bone = r
+		_update_chain()
 		notify_property_list_changed()
 
-@export var bone_tip: LaBone:
+## Last bone from the chain.[br][br]
+## [b]Note[/b]: It will not be affected by IK.
+@export var tip_bone: LaBone:
 	set(t):
-		bone_tip = t
-		chain = []
+		tip_bone = t
+		_update_chain()
 		notify_property_list_changed()
 
 @export var target: Node2D
 
-## Contains all nodes in the chain, starting from the bone_tip and going to the bone_root.[br]
-## It's easy to fill the array in this order and is the order which we calculate the IK.
-var chain: Array[LaBone]:
-	set(a):
-		if not bone_root:
-			chain = []
-			return
-		
-		if not bone_tip:
-			chain = []
-			return
-		
-		# We will walk from the bone_tip until find bone_root.
-		var current_bone: LaBone = bone_tip
-		chain = []
-		
-		while(current_bone):
-			chain.append(current_bone)
-			
-			# Finished with success because found the bone_root.
-			if current_bone == bone_root:
-				return
-			
-			current_bone = current_bone.get_parent()
-			
-			if not current_bone is LaBone:
-				break
-		
-		# Didn't found bone_root, so clear the array.
-		chain = []
-
-var order: Array[int] = []
+# Contains data about all bones from root_bone until tip_bone (not including it).
+var chain: Array[BoneData]
 
 
 func _get(property: StringName) -> Variant:
@@ -56,9 +28,16 @@ func _get(property: StringName) -> Variant:
 		var index = property.get_slice("/", 1).to_int()
 		var what = property.get_slice("/", 2)
 		
+		if index >= chain.size() or index < 0:
+			return null
+		
 		match what:
 			"bone":
-				return chain[index]
+				return chain[index].bone
+			"order":
+				return chain[index].order
+			"skip":
+				return chain[index].skip
 	
 	return null
 
@@ -80,7 +59,13 @@ func _get_property_list() -> Array[Dictionary]:
 			"type": TYPE_INT,
 			"usage": PROPERTY_USAGE_DEFAULT,
 			"hint": PROPERTY_HINT_RANGE,
-			"hint_string": "-1, %s" % (chain.size() - 1)
+			"hint_string": "0, %s" % (chain.size() - 1)
+		})
+		
+		property_list.append({
+			"name": "chain/%s/skip" % i,
+			"type": TYPE_BOOL,
+			"usage": PROPERTY_USAGE_DEFAULT,
 		})
 	
 	return property_list
@@ -91,8 +76,77 @@ func _set(property: StringName, value: Variant) -> bool:
 		var index = property.get_slice("/", 1).to_int()
 		var what = property.get_slice("/", 2)
 		
+		if index >= chain.size() or index < 0:
+			return false
+		
 		match what:
 			"bone":
-				pass
+				return false
+			"order":
+				_set_chain_order(index, value)
+				return true
+			"skip":
+				chain[index].skip = value
+				return true
 	
 	return false
+
+
+# Set the BoneData.order, but this means you have to switch
+# with the BoneData that had this order.
+func _set_chain_order(index: int, order: int) -> void:
+	var idx = -1
+	
+	for i in chain.size():
+		if chain[i].order == order:
+			idx = i
+			break
+	
+	if idx == -1:
+		return
+	
+	chain[idx].order = chain[index].order
+	chain[index].order = order
+
+
+func _update_chain() -> void:
+	if not root_bone:
+		chain = []
+		return
+	
+	if not tip_bone:
+		chain = []
+		return
+	
+	# Starting from tip_bone but not including it.
+	var parent = tip_bone.get_parent()
+	var index = 0
+	chain = []
+	
+	while(parent):
+		if not parent is LaBone:
+			break
+		
+		chain.append(BoneData.new(parent, index))
+		
+		# Finished with success because found the root_bone.
+		if parent == root_bone:
+			chain.reverse()
+			return
+		
+		parent = parent.get_parent()
+		index += 1
+	
+	# Didn't found root_bone, so clear the array.
+	chain = []
+
+
+# Data used in each bone during execution.
+class BoneData:
+	var bone: LaBone
+	var order: int
+	var skip: bool
+	
+	func _init(b: LaBone, o: int) -> void:
+		bone = b
+		order = o
